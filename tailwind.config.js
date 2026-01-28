fa82f4069e0ede0ab3c0b5ce119b2fc91d6b7b09
Simone Pedrazzi <pedra89@gmail.com>
2026-01-28 12:30:04 +0100

    fix: improve tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	content: ["./**/*.html", "./**/*.templ", "./**/*.go",],
+	content: ["./**/*.html", "./**/*.templ", "./**/*.go", "./web/assets/js/*.js"],
 	theme: { extend: {}, },
 	plugins: [],
 }
